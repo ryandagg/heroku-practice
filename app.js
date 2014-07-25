b678f0f30aa9d2229f6179b38d2083ef9b7c197f
ryandagg <ryan.dagg@gmail.com>
2014-07-25 09:31:15 -0600

    initial yo files

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+// 1) Modify the port to use whatever heroku gives us.
+var server = app.listen(process.env.port || 5403, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
